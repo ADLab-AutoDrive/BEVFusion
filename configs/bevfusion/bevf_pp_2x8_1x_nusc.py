3d77d69e1138f63b396b7d02eaf6c6582cdabd77
TingtingLiang <tingtingliang@pku.edu.cn>
2022-06-15 15:09:38 +0800

    fix lr

--- configs/bevfusion/bevf_pp_2x8_1x_nusc.py
+++ configs/bevfusion/bevf_pp_2x8_1x_nusc.py
@@ -144,7 +144,7 @@ data = dict(
     samples_per_gpu=2,
     workers_per_gpu=6,)
 
-optimizer = dict(type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+optimizer = dict(type='AdamW', lr=0.001, betas=(0.9, 0.999), weight_decay=0.05,
                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
                                                  'relative_position_bias_table': dict(decay_mult=0.),
                                                  'norm': dict(decay_mult=0.)}))
